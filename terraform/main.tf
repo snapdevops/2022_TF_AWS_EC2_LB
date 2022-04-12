30723bb11967e497c1e7d12733b686285d77bdf8
snapdevops <46382167+snapdevops@users.noreply.github.com>
2022-04-11 21:45:49 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,10 +37,27 @@ resource "aws_network_interface" "omc_network_interface" {
 resource "aws_instance" "my_tf_ec2_instance" {
   ami           = "ami-0b0ea68c435eb488d" # us-west-2
   instance_type = var.list[1]
+  
+  provisioner "remote-exec  {
+     inline = [
+      "sudo amazon-linux-extras install -y nginx1.12"
+      "sudo systemctl start nginx"
+     
+     ]
+  }
+  
+  
+  module "key_pair" {
+  source = "terraform-aws-modules/key-pair/aws"
+
+  key_name   = "deployer-two"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
+
+}
 
   
   
-  
+
   network_interface {
     network_interface_id = aws_network_interface.omc_network_interface.id
     device_index         = 0
